b2a362dfacd457c3f2d915ab2f2fbf13c5e076ff
Jason Bell <jasonbell143@gmail.com>
2024-09-16 16:58:19 +0100

    extracted parameter from endpoint

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,3 +1,13 @@
-export function Article({ article }) {
-  console.log(article);
+import { useParams, useSearchParams } from "react-router-dom";
+
+export function Article() {
+  const params = useParams();
+  const article_id = Number(params.article_id);
+  console.log({ article_id });
+  return (
+    <>
+      <p> You're seeing a specific article MODIFY</p>
+      <p>With ID {article_id}</p>
+    </>
+  );
 }
